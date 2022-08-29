7f9dc9e5120441eac0df49c1fa85b6370af3c93a
uwenorjeff <jeffrey_uwenor@yahoo.com>
2022-08-29 05:38:19 -0700

    shell_project

---
+++ command_line_to_av.c
@@ -0,0 +1,33 @@
+/*a function that splits a string and returns an array of each word of the string*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main-prototype
+ * Return: 0
+ */
+
+int main(void)
+
+{
+char *buffer = malloc(1024);
+size_t len = 1024;
+char *token;
+
+while (1)
+
+{
+printf("$ ");
+getline(&buffer, &len, stdin);
+printf("buffer getline: %s", buffer);
+}
+token = strtok(buffer, " ");
+while (token)
+{
+printf("buffer strtok: %s\n", token);
+token = strtok(NULL, " ");
+}
+return (0);
+}
